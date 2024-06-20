2c49617af8a9e80ca43728f85e87013ee407d166
Kaio <kaiogabrielrozini@gmail.com>
2024-06-20 19:42:23 -0300

    adicionado a criação completa da adaga de turmalina

--- src/main/java/net/kaio/bituksmod/item/ModItems.java
+++ src/main/java/net/kaio/bituksmod/item/ModItems.java
@@ -20,6 +20,8 @@ public class ModItems {
     public static final RegistryObject<Item> RAW_TURMALINA = ITEMS.register("raw_turmalina", () -> new Item(new Item.Properties().tab(ModeCreativeModelTab.BITUKS_TAB)));
     public static final RegistryObject<Item> TURMALINA_CRISTAL = ITEMS.register("turmalina_cristal", () -> new Item(new Item.Properties().tab(ModeCreativeModelTab.BITUKS_TAB)));
     public static final RegistryObject<Item> TURMALINA_PEARL = ITEMS.register("turmalina_pearl", () -> new Item(new Item.Properties().tab(ModeCreativeModelTab.BITUKS_TAB)));
+    public static final RegistryObject<Item> DAGGER_HANDLE = ITEMS.register("dagger_handle", () -> new Item(new Item.Properties().tab(ModeCreativeModelTab.BITUKS_TAB)));
+    public static final RegistryObject<Item> RED_ACACIA_STICK = ITEMS.register("red_acacia_stick", () -> new Item(new Item.Properties().tab(ModeCreativeModelTab.BITUKS_TAB)));
     public static final RegistryObject<Item> TURMALINA_SEEDS = ITEMS.register("turmalina_seeds", () -> new ItemNameBlockItem(ModBlocks.TURMALINA_CROP.get(),
             new Item.Properties().tab(ModeCreativeModelTab.BITUKS_TAB)));
 
